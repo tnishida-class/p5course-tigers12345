9d6993c858bd55dc8875823e114b1d056d27a7b0
tigers12345 <91927250+tigers12345@users.noreply.github.com>
2021-10-05 12:58:48 +0900

    change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(111, 111, 111);
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);

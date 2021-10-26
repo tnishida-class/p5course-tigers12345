1b4de155fd7ded6f0411065eb3000f52214a42b1
tigers12345 <91927250+tigers12345@users.noreply.github.com>
2021-10-26 15:51:52 +0900

    section3-q1 change

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -12,5 +12,17 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
-}
+
+  if(count < 50 ) {
+    ellipse(width / 2, height / 2, count);
+    fill(255,0,0);
+  }
+  else if(count=>50){
+  ellipse(width / 2, height / 2, 100-count);
+  fill(0,255,0)
+  }
+  if(keyIsDown(LEFT_ARROW)){ width -= 2; }
+ if(keyIsDown(RIGHT_ARROW)){ width+= 2; }
+if(keyIsDown(UP_ARROW)){count=count+1}
+if(keyIsDown(DOWN_ARROW)){count=count-1/2}
+  }

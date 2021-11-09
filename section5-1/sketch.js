01928f31d304ff2097ad84070b4a86447487b73e
tigers12345 <91927250+tigers12345@users.noreply.github.com>
2021-11-09 23:46:01 +0900

    section5-1changes

--- section5-1/sketch.js
+++ section5-1/sketch.js
@@ -7,8 +7,18 @@ function setup(){
     let theta = TWO_PI * i / 12;
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+     star(x, y, 10);
   }
 }
 
 // ヒント：section5-2 にある star 関数をここにコピーして、 draw 内で ellipse の代わりに使おう
+function star(cx, cy, r){
+  beginShape();    // 点つなぎを始める
+  for(let i = 0; i < 5; i++){
+    const theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    const x = cx + cos(theta) * r;
+    const y = cy + sin(theta) * r;
+    vertex(x, y);  // 次につなぐ点を１つ増やす
+  }
+  endShape(CLOSE); // 点つなぎを終わる
+}
